c32318cbb5bddf416ed7a7032328ce02ad51e445
Aysenur Sahin <aysenursahinxx@gmail.com>
2020-10-13 13:18:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class SwapExample
+{
+    public static void Main(string[] args)
+    {
+        int a = 5, b = 10;
+        Console.WriteLine("Sayıların Yer Değiştirmeden Önceki Değerleri a= " + a + " b= " + b);
+        a = a * b;       
+        b = a / b;
+        a = a / b;    
+        Console.Write("Sayıların Yer Değiştirdikten Sonraki Değerleri  a= " + a + " b= " + b);
+        Console.ReadLine();
+    }
+}
